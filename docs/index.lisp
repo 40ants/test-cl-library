a40d572256b0c85ee8ef1f2f158151280bf4bc7f
Alexander Artemenko <svetlyak.40wt@gmail.com>
2023-02-08 01:26:56 +0300

    My theme again.

--- docs/index.lisp
+++ docs/index.lisp
@@ -21,6 +21,17 @@
 
 
 (defmethod docs-config ((system (eql (asdf:find-system "test-cl-lib-docs"))))
+  ;; 40ANTS-DOC-THEME-40ANTS system will bring
+  ;; as dependency a full 40ANTS-DOC but we don't want
+  ;; unnecessary dependencies here:
+  #+quicklisp
+  (ql:quickload "40ants-doc-theme-40ants")
+  #-quicklisp
+  (asdf:load-system "40ants-doc-theme-40ants")
+  
+  (list :theme
+        (find-symbol "40ANTS-THEME"
+                     (find-package "40ANTS-DOC-THEME-40ANTS")))
   )
 
 
